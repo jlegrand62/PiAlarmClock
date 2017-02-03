5d8010239ae78aca855452e09a0229ef4d5767bb
Adam Krog <akrog100@gmail.com>
2017-02-02 22:14:49 -0500

    Added dynamically updating clock label to main screen of the app.  Added placeholder label to settings screen.  Played with sizing and position of transitions and buttons

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
@@ -9,6 +10,18 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 import time
 from kivy.uix.label import Label
 
+kivy.require("1.9.1")
+
+class ClockLabel(Label):
+    def __init__(self, **kwargs):
+        super(ClockLabel, self).__init__(**kwargs)
+        self.text = str(time.asctime())
+        Clock.schedule_interval(self.update, 1)
+
+    def update(self, *args):
+        self.text = str(time.asctime())
+        
+
 class ClockScreen(Screen):
     pass
 
@@ -18,11 +31,11 @@ class SettingsScreen(Screen):
 class ScreenHandler(ScreenManager):
     pass
 
-app = Builder.load_file("meza.kv")
-
 class MezaApp(App):
 
     def build(self):
+        app = Builder.load_file("meza.kv")
+        crudeclock = ClockLabel()
         return app
 
 if __name__ == '__main__':
