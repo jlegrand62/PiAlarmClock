f0b027db366baf301f7a39f8ce7001eabac1bff4
Adam Krog <adam.b.krog@gmail.com>
2017-02-24 09:49:06 -0500

    Added set alarm button to alarm popup that stores alarm minute and hour as global variables to be used for smartsleep

--- main.py
+++ main.py
@@ -17,12 +17,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.dropdown import DropDown
+from functools import partial
 
 
 kivy.require("1.9.1")
 smart_sleep = 0
 weather_stat = 0
 news_stat = 0
+alarm_hour = 0
+alarm_minute = 0
 
 class WeatherStatusLabel(Label):
     def __init__(self, **kwargs):
@@ -46,12 +49,16 @@ class SetAlarmButton(Button):
 
     def alarmPopup(self):
         box = FloatLayout()
-        
+
+        #hour selector
         hourbutton = Button(text='Select Hour', size_hint=(.2,.2),
                             pos_hint={'x':.2, 'y':.5})
         hourdropdown = DropDown()
         for i in range(24):
-            btn=Button(text = '%r' % i, size_hint_y=None, height =30)
+            if(i<10):
+                btn=Button(text = '0%r' % i, size_hint_y=None, height =30)
+            else:
+                btn=Button(text = '%r' % i, size_hint_y=None, height =30)
             btn.bind(on_release=lambda btn: hourdropdown.select(btn.text))
             hourdropdown.add_widget(btn)
 
@@ -60,24 +67,46 @@ class SetAlarmButton(Button):
         box.add_widget(hourbutton)
         box.add_widget(hourdropdown)
 
+        #minute selector
         minutebutton = Button(text='Select Minute', size_hint=(.2,.2),
                             pos_hint={'x':.6, 'y':.5})
         minutedropdown = DropDown()
         for i in range(60):
-            btn=Button(text = '%r' % i, size_hint_y=None, height =30)
+            if(i<10):
+                btn=Button(text = '0%r' % i, size_hint_y=None, height =30)
+            else:
+                btn=Button(text = '%r' % i, size_hint_y=None, height =30)
             btn.bind(on_release=lambda btn: minutedropdown.select(btn.text))
             minutedropdown.add_widget(btn)
-
+        
         minutebutton.bind(on_release=minutedropdown.open)
         minutedropdown.bind(on_select=lambda instance, x: setattr(minutebutton, 'text', x))
         box.add_widget(minutebutton)
         box.add_widget(minutedropdown)
+
+        dismissButton = PopupDismissButton()
+        box.add_widget(dismissButton)
         
-        alarmPopup = Popup(title='Set Your Alarm:',
-                    content=box,
-                    size_hint=(.9, .9))
+        
+        alarmPopup = Popup(title='Set Your Alarm:', content=box, size_hint=(.9, .9))
+        dismissButton.bind(on_press=partial(dismissButton.dismissPopup, alarmPopup, hourbutton, minutebutton))
         alarmPopup.open()
-        
+
+class PopupDismissButton(Button):
+    def __init__(self, **kwargs):
+        super(PopupDismissButton, self).__init__(**kwargs)
+        self.text = "Set Alarm"
+        self.size_hint=(.2,.2);
+        self.pos_hint={'x':.5, 'y':.2}
+
+    def dismissPopup(self, instance, button1, button2, button3):
+        global alarm_hour
+        global alarm_minute
+
+        alarm_hour = button1.text
+        alarm_minute = button2.text
+
+        instance.dismiss()
 
 class ClockLabel(Label):
     def __init__(self, **kwargs):
