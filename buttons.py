9a2486ac230d832e54049c7e6084e1fcbc531714
jlegrand62 <jlegrand62@gmail.com>
2020-01-10 09:44:33 +0100

    import debug

--- buttons.py
+++ buttons.py
@@ -16,7 +16,7 @@ from kivy.uix.dropdown import DropDown
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 
-from Meza.main import STORE
+from main import STORE
 
 
 class PopupDismissButton(Button):
